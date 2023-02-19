64550805ecf9fe80957c0c32f7262180f82684ef
Alex Kukri <sairkuk@gmail.com>
2023-02-19 18:29:15 +0200

    add file with lesson

--- cypress/support/helper.js
+++ cypress/support/helper.js
@@ -1,20 +1,35 @@
-
-
-function loginViaUI(user){
+export function loginViaUI(user){
     cy.log('**Open website login page**');
     cy.visit('/');
 
     cy.log('**Authorize user**');
     cy.get('#customer_menu_top').click();
-    cy.get('#loginFrm_loginname').type(user.userName);
+    cy.get('#loginFrm_loginname').type(user.username);
     cy.get('#loginFrm_password').type(user.password);
     cy.get('[title="Login"]').click();
     cy.get('h1 span.subtext').should('contain', user.firstName);
-
-
-
 }
 
-export function headlaessLogin(user){
-    
+export function headlessLogin(user){
+    let csrfToken;
+    let csrfInstance;
+
+    cy.request('GET', '/index.php?rt=account/login').then( response => {
+        let htmlResp = document.createElement('html')
+        htmlResp.innerHTML = response.body;
+        csrfToken = htmlResp.querySelector('#loginFrm [name="csrftoken"]').getAttribute('value');
+        csrfInstance = htmlResp.querySelector('#loginFrm [name="csrfinstance"]').getAttribute('value');
+    }).then(() => {
+        cy.request({
+            method: 'POST',
+            url: '/index.php?rt=account/login',
+            body: {
+                loginname: user.username,
+                password: user.password,
+                csrfinstance: csrfInstance,
+                csrftoken: csrfToken
+            },
+            form: true
+        })
+    })
 }
